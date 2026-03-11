ef34f44c1ec3f2176442498c78b003c94803c1c1
Alex Vergara Gil <alex.vergara@biomediqa.com>
2026-03-11 16:18:24 +0100

    fix

--- .github/workflows/actions_build/ci_build_wheel_ubuntu.sh
+++ .github/workflows/actions_build/ci_build_wheel_ubuntu.sh
@@ -37,11 +37,11 @@ export CIBW_PLATFORM="linux"
 export CIBW_REPAIR_WHEEL_COMMAND_LINUX=""
 export CIBW_SKIP="*-musllinux_*"
 export CIBW_ENVIRONMENT="
-export PATH=/software/cmake/cmake/bin/:${PATH} &&
-source /software/geant4/bin/geant4make.sh &&
-export CMAKE_PREFIX_PATH=/software/geant4/bin:/software/itk/bin/:${CMAKE_PREFIX_PATH} &&
-. /opt/rh/gcc-toolset-14/enable"
+PATH=/software/cmake/cmake/bin/:${PATH} &&
+CMAKE_PREFIX_PATH=/software/geant4/bin:/software/itk/bin/:${CMAKE_PREFIX_PATH}"
 export CIBW_BEFORE_BUILD="
+cat /software/geant4/bin/geant4make.sh &&
+. /opt/rh/gcc-toolset-14/enable &&
 mkdir opengate_core/plugins &&
 cp -r /lib64/qt6/plugins/platforms/* opengate_core/plugins/ && 
 cp -r /lib64/qt6/plugins/imageformats opengate_core/plugins/ &&
