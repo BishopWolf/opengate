5077ad43b2f55312c1e7a576268da627bc382a4e
Alex Vergara Gil <alex.vergara@biomediqa.com>
2026-03-13 11:37:24 +0100

    wrong build image

--- .github/workflows/actions_build/ci_build_wheel_ubuntu.sh
+++ .github/workflows/actions_build/ci_build_wheel_ubuntu.sh
@@ -13,20 +13,21 @@ pip install wget colored setuptools
 
 # Setup the environment for the build
 if [ ${MATRIX_OS} == "ubuntu-24.04-arm" ]; then
+  export ARMDOCKER="_arm64"
   export CIBW_ARCHS="aarch64"
   export CIBW_MANYLINUX_AARCH64_IMAGE=tbaudier/opengate_core:${GEANT4_VERSION}$ARMDOCKER
 else
   export CIBW_ARCHS="x86_64"
-  export CIBW_MANYLINUX_AARCH64_IMAGE=tbaudier/opengate_core:${GEANT4_VERSION}
+  export CIBW_MANYLINUX_X86_64_IMAGE=tbaudier/opengate_core:${GEANT4_VERSION}
 fi
 export CIBW_PLATFORM="linux"
 export CIBW_REPAIR_WHEEL_COMMAND_LINUX=""
 export CIBW_SKIP="*-musllinux_*"
 export CIBW_BEFORE_BUILD="
-python -m pip install colored
-mkdir opengate_core/plugins
-export QT_PLUGIN_DIR=$(qtpaths6 --plugin-dir)
-cp -r $QT_PLUGIN_DIR/platforms/* opengate_core/plugins/
+python -m pip install colored &&
+mkdir opengate_core/plugins &&
+export QT_PLUGIN_DIR=$(qtpaths6 --plugin-dir) &&
+cp -r $QT_PLUGIN_DIR/platforms/* opengate_core/plugins/ &&
 cp -r $QT_PLUGIN_DIR/imageformats opengate_core/plugins/
 "
 
@@ -36,8 +37,6 @@ CMAKE_PREFIX_PATH=/software/geant4/install:/software/itk/install
 Geant4_DIR=/software/geant4/install/lib/cmake/Geant4
 ITK_DIR=/software/itk/install/lib/cmake/ITK
 LD_LIBRARY_PATH=/software/geant4/install/lib:/software/itk/install/lib
-QT_PLUGIN_PATH=$QT_PLUGIN_DIR
-QT_QPA_PLATFORM_PLUGIN_PATH=$QT_PLUGIN_DIR/platforms
 "
 
 # Run the build without docker
