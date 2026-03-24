6efd2c9bdfb38650dae3d4ecdb63552f9618b0b1
Alex Vergara Gil <alex.vergara@biomediqa.com>
2026-03-24 15:54:18 +0100

    ensure python folder exists before installation

--- .github/workflows/actions_build/ci_build_wheel_macos.sh
+++ .github/workflows/actions_build/ci_build_wheel_macos.sh
@@ -5,9 +5,11 @@ source $GITHUB_WORKSPACE/env_dump.txt
 mkdir -p $HOME/software
 
 brew install python
-python3 -m venv $HOME/software/opengate_core
+python3 -m venv $HOME/software/opengate_core --python ${MATRIX_PYTHON_VERSION}
 source $HOME/software/opengate_core/bin/activate
-$HOME/software/opengate_core/bin/pip install wget colored delocate cibuildwheel[uv]==3.4.0
+echo "$HOME/software/opengate_core/bin" >> $GITHUB_PATH
+pip install wget colored delocate cibuildwheel[uv]==3.4.0
+mkdir -p /Library/Frameworks/Python.framework/Versions/${MATRIX_PYTHON_VERSION}/bin
 
 export LDFLAGS="-L/usr/local/opt/llvm/lib"
 export CPPFLAGS="-I/usr/local/opt/llvm/include -fopenmp"
@@ -65,7 +67,7 @@ if [[ ${MATRIX_OS} == "macos-15-intel" ]]; then
 else
     export DYLD_LIBRARY_PATH=$HOME/software/geant4/bin/BuildProducts/lib:/opt/homebrew/share/qt/plugins/platforms/:/opt/X11/lib/:$DYLD_LIBRARY_PATH:/opt/homebrew/lib
     export CIBW_ARCHS_MACOS="arm64"
-    $HOME/software/opengate_core/bin/python -c "import os,delocate; print(os.path.join(os.path.dirname(delocate.__file__), 'tools.py'));quit()" | xargs -I{} sed -i."" "s/first, /input.pop('i386',None); first, /g" {}
+    python -c "import os,delocate; print(os.path.join(os.path.dirname(delocate.__file__), 'tools.py'));quit()" | xargs -I{} sed -i."" "s/first, /input.pop('i386',None); first, /g" {}
 fi
 if [[ ${MATRIX_PYTHON_VERSION} == "3.10" ]]; then
   export CIBW_BUILD="cp310-*"
@@ -79,7 +81,7 @@ elif [[ ${MATRIX_PYTHON_VERSION} == "3.14" ]]; then
   export CIBW_BUILD="cp314-*"
 fi
 
-$HOME/software/opengate_core/bin/python -m cibuildwheel --output-dir dist
+python -m cibuildwheel --output-dir dist
 cd dist
 if [[ ${MATRIX_OS} == "macos-15-intel" ]]; then
     find . -name '*whl' -exec bash -c ' mv $0 ${0/macosx_15_0/macosx_10_9}' {} \;
